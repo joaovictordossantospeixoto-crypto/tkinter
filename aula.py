c1cfa88254fef81eeccbc39f26cfd1bb2379a81d
Victor <joaovictordossantospeixoto@gmail.com>
2026-04-17 13:19:46 -0300

    Alterações no projeto tkinter

--- aula.py
+++ aula.py
@@ -1,5 +1,11 @@
 import tkinter as tk
 app = tk.Tk()
+def clicakar():
+    print("Clicou no botão")
 app.title("Olá Mundo")
 app.geometry("300x300")
+Label = tk.Label(app, text="Bem Vindo ao Tkinter")
+Label.pack(pady=20)
+Button = tk.Button(app, text="Clique aqui", command=clicakar)
+Button.pack(pady=10)
 app.mainloop()
