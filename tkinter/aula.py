edec799fad7cc8efe0812db8f8d848509c3ae911
joão <joaovictordossantospeixoto@gmail.com>
2026-04-28 21:35:41 -0300

    Corrigido: removido repositório git interno

---
+++ tkinter/aula.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+app = tk.Tk()
+def clicakar():
+    print("Clicou no botão")
+app.title("Olá Mundo")
+app.geometry("400x300")
+app.configure(bg="#345678")
+app.resizable(True,False)
+Label = tk.Label(app,
+                 border="5",
+                 background="#FFFFFF",
+                 foreground="#000000",
+                 text="Bem Vindo ao Tkinter")
+Label.pack(pady=20)
+Button = tk.Button(app,background="#86C3FF", text="Clique aqui", command=clicakar)
+Button.pack(pady=10)
+
+app.mainloop()
